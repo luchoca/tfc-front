f459b9863ee9e6f469011f519cfee52fe4e1df0e
luchoca <luchocastro.3@gmail.com>
2023-03-27 22:45:28 -0300

    Se corrigen estilos en header y login(client) 2.0

--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client.component';
 import { MaterialModule } from '../material/material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ClientComponent],
-  imports: [MaterialModule, CommonModule],
+  imports: [MaterialModule, CommonModule, RouterModule],
   exports: [ClientComponent],
 })
 export class ClientModule {}
