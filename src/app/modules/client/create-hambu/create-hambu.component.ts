aa55ab1c970c91f39bf7cd61298b5e740387bdad
luchoca <luchocastro.3@gmail.com>
2023-04-20 14:07:40 -0300

    Se hardcodea ingredientes de cada tabla CreateHambu

--- src/app/modules/client/create-hambu/create-hambu.component.ts
+++ src/app/modules/client/create-hambu/create-hambu.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
-export interface PeriodicElement {
+export interface IngredientesSchema {
   name: string;
   precio: number;
   symbol: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
+const VEGETALES_DATA: IngredientesSchema[] = [
   { name: 'Lechuga', precio: 10, symbol: 'H' },
   { name: 'Pepinillos', precio: 25, symbol: 'He' },
   { name: 'Cebolla', precio: 20, symbol: 'Li' },
   { name: 'Tomate', precio: 20, symbol: 'Be' },
   { name: 'Cibullete', precio: 10, symbol: 'B' },
+];
+const PROTEINA_DATA: IngredientesSchema[] = [
+  { name: 'Carne', precio: 10, symbol: 'H' },
+  { name: 'Pollo', precio: 25, symbol: 'He' },
+  { name: 'Vegetariana', precio: 20, symbol: 'Li' },
+];
+const TOPICS_DATA: IngredientesSchema[] = [
   { name: 'Huevo', precio: 30, symbol: 'C' },
   { name: 'Queso', precio: 40, symbol: 'N' },
   { name: 'Panceta', precio: 30, symbol: 'O' },
+];
+const SALSAS_DATA: IngredientesSchema[] = [
   { name: 'Mayonesa', precio: 20, symbol: 'F' },
   { name: 'Mostaza', precio: 20, symbol: 'Ne' },
 ];
@@ -23,5 +32,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CreateHambuComponent {
   displayedColumns: string[] = ['name', 'precio', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  proteinas = PROTEINA_DATA;
+  vegetales = VEGETALES_DATA;
+  topics = TOPICS_DATA;
+  salsas = SALSAS_DATA;
 }
