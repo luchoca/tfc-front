8479e6b9835e29d8220850e6c1f409a19c406eb6
luchoca <luchocastro.3@gmail.com>
2023-05-09 22:42:05 -0300

    Se deja batante funcional , con video aun, se crea componente carrusel

---
+++ src/app/shared/carrusel/carrusel.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarruselComponent } from './carrusel.component';
+
+@NgModule({
+  declarations: [CarruselComponent],
+  imports: [CommonModule],
+  exports: [CarruselComponent],
+})
+export class CarruselModule {}
